113e3786550b2e522f4e00f1c7f0d04ee8134711
thiagoHenrique847 <thiagocontato1232@gmail.com>
2024-10-23 21:20:24 -0300

    Att

--- livraria_api/books.js
+++ livraria_api/books.js
@@ -17,7 +17,7 @@ router.post('/', async (req, res) => {
 
 });
 
-router.get('/', async (req, res) => {   
+router.get('/get', async (req, res) => {   
     try {
         const books = await Book.find();
         res.status(200).json(books)
