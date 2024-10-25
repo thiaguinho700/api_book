3e2f9e99363800a53778ecc21047a02d88fa79de
thiaguinho700 <thiagobreakgo@gmail.com>
2024-10-24 21:20:13 -0300

    Att

--- livraria_api/books.js
+++ livraria_api/books.js
@@ -19,10 +19,10 @@ router.post('/', async (req, res) => {
 
 router.get('/get', async (req, res) => {   
     try {
-        // const books = await Book.find();
-        // res.status(200).json(books)
-        
+        const books = await Book.find();
         console.log("Sucesso");
+        res.status(200).json(books)
+        
         
     } catch (error) {
         res.status(500).json({ message: 'Erro ao buscar os livros ', error })
